82c45eb0dfcc0f9c9145947243fa29340e1f3276
jial <jialei28@126.com>
2016-11-17 15:42:30 +0800

    project create.

---
+++ src/org/world/meta/unit.java
@@ -0,0 +1,10 @@
+package org.world.meta;
+
+public class unit {
+
+	public unit() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
---
+++ test/worldtest.java
@@ -0,0 +1,9 @@
+
+public class worldtest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
